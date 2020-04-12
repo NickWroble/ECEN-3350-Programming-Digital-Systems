16c45028024a0884e6a3f66c06737524e56e6f64
NickWroble <45834446+NickWroble@users.noreply.github.com>
2020-04-11 21:19:57 -0400

    Update part1.s

--- project3/part1.s
+++ project3/part1.s
@@ -9,7 +9,7 @@ ret
 .global op_three
 op_three:
 #a is r4, b is r5, c is r6
-    subi sp, sp, 12 #allocating space on the stack for frame pointer and stack pointer
+    subi sp, sp, 12 #allocating space on the stack for frame pointer and stack pointer and return address
     stw ra, 8(sp)
     stw fp, 4(sp)
     addi fp, sp, 4
@@ -20,17 +20,23 @@ op_three:
     mov r5, r6 #moving c into the second argument register
     call op_two #operates on r4 and r5 (return value and c)
 
-    ldw ra, 8(sp) #restroing the stack and the frame
+    ldw ra, 8(sp) #restroing stack frame and return address
     ldw fp, 4(sp)
     addi sp, sp, 12
 ret
 
 .global fibonacci
 fibonacci:
-    
+#r4 is n
+    mov r8, r4
+    mov r10, r0
+    fibonacci_intermediate:
+    subi r9, r8, 1
+    add r10, r8, r9
 
-    subi r4, r4, 1
-    bgt r4, r0, fibonacci
+    
+    bgt r4, r0, fibonacci_intermediate
+    
 ret
 
 .end
